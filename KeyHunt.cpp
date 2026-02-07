17924a0529563526a53c4eca5993b92fafb8566b
Anonymous Person <xjfkf323@gmail.com>
2026-02-07 12:53:42 +0530

    Update KeyHunt.cpp

--- KeyHunt.cpp
+++ KeyHunt.cpp
@@ -1011,7 +1011,7 @@ avgKeyRate /= (double)(nbSample);
 avgGpuKeyRate /= (double)(nbSample);
 if (isAlive(params)) {
 memset(timeStr, '\0', 256);
-printf("\r[%s] [GPU: %.2f Mk/s] [P: %lf %%] [%d bit] [F: %d] [R: %"PRIu64 "]",
+printf("\r[%s] [GPU: %.2f Mk/s] [P: %lf %%] [2^%d] [F: %d]",
 toTimeStr(t1, timeStr),
 avgGpuKeyRate / 1000000.0,
 completedPerc,
