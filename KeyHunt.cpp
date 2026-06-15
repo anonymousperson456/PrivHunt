478ebc44328a68c03a25a185818233a59a600644
Anonymous Person <xjfkf323@gmail.com>
2026-06-15 12:33:08 +0530

    Update KeyHunt.cpp

--- KeyHunt.cpp
+++ KeyHunt.cpp
@@ -1019,7 +1019,7 @@ if (isAlive(params)) {
   double displayRate;
   const char* unit;
   double stopPerc = CalcPercantage(ICount, rangeStart, rangeDiff2);
-  if (stopPerc >= 98.860677) stopPerc = 100.0;
+  if (stopPerc >= 100.0) stopPerc = 100.0;
   if (avgGpuKeyRate >= 1e12) {
     displayRate = avgGpuKeyRate / 1e12;
     unit = "TK/s";
