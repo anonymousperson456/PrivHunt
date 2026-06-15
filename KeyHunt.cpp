e84050dd3bb5ce2fb2ac69ea462894b386326151
Anonymous Person <xjfkf323@gmail.com>
2026-06-15 09:29:16 +0530

    Correct stopPerc comparison in exit condition
    Fix comparison operator in exit condition.

--- KeyHunt.cpp
+++ KeyHunt.cpp
@@ -1051,7 +1051,7 @@ lastGPUCount = gpuCount;
 t0 = t1;
 
 double stopPerc = CalcPercantage(ICount, rangeStart, rangeDiff2);
-if (should_exit || nbFoundKey >= targetCounter || stopPerc >= 100.0)
+if (should_exit || nbFoundKey >= targetCounter || stopPerc = 100.0)
     endOfSearch = true;
 }
 printf("\n");
